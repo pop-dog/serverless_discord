c8fc66ee1bc00f37c0fdacbab97550f8aabd241a
William Myers <wmyers09@gmail.com>
2023-05-26 18:32:25 -0400

    Changed env variable name for chatbot sqs url.

--- lambda_bot/commands/chat.js
+++ lambda_bot/commands/chat.js
@@ -7,7 +7,7 @@ module.exports = async function (body) {
     if (!data || !data.options || data.options.length != 1 || data.options[0].name != 'message') return 'Invalid options for /chat command.';
     
     try {
-        let sqsResponse =  await awsHelper.sqs.sendMessage(process.env.SQS_QUEUE_URL, { message: data.options[0].value });
+        let sqsResponse =  await awsHelper.sqs.sendMessage(process.env.CHATBOT_QUEUE_URL, { message: data.options[0].value });
         console.log(sqsResponse);
     }
     catch (ex) {
