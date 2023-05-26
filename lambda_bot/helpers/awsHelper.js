a135b4ff9afb7859cb451dab3d4aae40abbac957
William Myers <wmyers09@gmail.com>
2023-05-26 18:34:06 -0400

    Added additional logging.

--- lambda_bot/helpers/awsHelper.js
+++ lambda_bot/helpers/awsHelper.js
@@ -5,6 +5,7 @@ module.exports = {
     sqs: {
         // Send a message to the SQS queue
         sendMessage: async function (queueUrl, message) {
+            console.log(`Sending message to SQS queue ${queueUrl}: ${JSON.stringify(message)}`);
             const sqs = new AWS.SQS();
             const params = {
                 MessageBody: JSON.stringify(message),
