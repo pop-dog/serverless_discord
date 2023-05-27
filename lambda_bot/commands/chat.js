7041bb1f8f93218e4b004361be81aa31894cefa5
William Myers <wmyers09@gmail.com>
2023-05-26 23:32:53 -0400

    Trying async again.

--- lambda_bot/commands/chat.js
+++ lambda_bot/commands/chat.js
@@ -1,21 +1,22 @@
 const axios = require('axios').default;
 
-module.exports = function (body) {
+module.exports = async function (body) {
     const data = body.data;
 
     // Validate the request
     if (!data || !data.options || data.options.length != 1 || data.options[0].name != 'message') return 'Invalid options for /chat command.';
-    
-    try {
-        console.log(`Sending chat message: "${data.options[0].value}" to ${process.env.CHAT_URL}`);
-        axios.post(process.env.CHAT_URL, {
-            message: data.options[0].value,
-            chat_auth_id: process.env.CHAT_AUTH_ID,
-        });
-    }
-    catch (ex) {
-        console.log(ex);
-        return 'Error sending chat message.';
-    }
-    return '';
+
+    console.log(`Sending chat message: "${data.options[0].value}" to ${process.env.CHAT_URL}`);
+    axios.post(process.env.CHAT_URL, {
+        message: data.options[0].value,
+        chat_auth_id: process.env.CHAT_AUTH_ID,
+    })
+    .then(function (response) {
+        console.log(response);
+        return '';
+      })
+      .catch(function (error) {
+        console.log(error);
+        return 'Error sending message to chat bot.'
+      });
 }
--- lambda_bot/helpers/slash-command-handler.js
+++ lambda_bot/helpers/slash-command-handler.js
@@ -1,7 +1,7 @@
 const roll = require('../commands/roll.js');
 const chat = require('../commands/chat.js');
 
-module.exports = function(body) {
+module.exports = async function(body) {
   const data = body.data;
 
   // Validate the request
@@ -14,7 +14,7 @@ module.exports = function(body) {
     case 'roll':
       return interactionResponse(roll(body));
     case 'chat':
-      let chatResponse = chat(body);
+      let chatResponse = await chat(body);
       if (!chatResponse) return interactionResponse('Message sent.', 5);
       throw chatResponse; 
     default:
