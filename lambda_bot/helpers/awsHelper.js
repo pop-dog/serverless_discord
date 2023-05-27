1ac239c72752065e1f0f85f34c8759c4ae05a8ad
William Myers <wmyers09@gmail.com>
2023-05-27 12:06:54 -0400

    Updated SQS call to wait for response.

--- lambda_bot/helpers/awsHelper.js
+++ lambda_bot/helpers/awsHelper.js
@@ -4,14 +4,14 @@ module.exports = {
     // SQS helper functions
     sqs: {
         // Send a message to the SQS queue
-        sendMessage: function (queueUrl, message) {
+        sendMessage: async function (queueUrl, message) {
             console.log(`Sending message to SQS queue ${queueUrl}: ${JSON.stringify(message)}`);
             const sqs = new AWS.SQS();
             const params = {
                 MessageBody: JSON.stringify(message),
                 QueueUrl: queueUrl
             };
-            sqs.sendMessage(params);
+            sqs.sendMessage(params).promise();
         }
     }
 }
