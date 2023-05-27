21f5a61749641eaa5bf6f19683d95371a76276fe
William Myers <wmyers09@gmail.com>
2023-05-27 11:47:22 -0400

    Switched back to SQS

--- lambda_bot/commands/chat.js
+++ lambda_bot/commands/chat.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+const awsHelper = require('../helpers/awsHelper');
 
 module.exports = async function (body) {
     const data = body.data;
@@ -6,17 +6,16 @@ module.exports = async function (body) {
     // Validate the request
     if (!data || !data.options || data.options.length != 1 || data.options[0].name != 'message') return 'Invalid options for /chat command.';
 
-    console.log(`Sending chat message: "${data.options[0].value}" to ${process.env.CHAT_URL}`);
-    axios.post(process.env.CHAT_URL, {
-        message: data.options[0].value,
-        chat_auth_id: process.env.CHAT_AUTH_ID,
-    })
-    .then(function (response) {
-        console.log(response);
-        return '';
-      })
-      .catch(function (error) {
-        console.log(error);
-        return 'Error sending message to chat bot.'
+    try {
+      // Send SQS message
+      let sqsResponse = await awsHelper.sqs.sendMessage(process.env.CHATBOT_QUEUE_URL, {
+          message: data.options[0].value,
       });
+      console.log(sqsResponse);
+      return '';
+    } catch(ex) {
+      console.log(ex);
+      return 'Failed to send message to SQS queue.';
+    }
+
 }
